399ce1f76688d3b4550f5831d21fe6765b68bf0c
Parad0x <viktor3755@gmail.com>
2021-12-08 21:12:51 +0300

    acc

--- src/main/java/com/example/demo5/Account.java
+++ src/main/java/com/example/demo5/Account.java
@@ -4,11 +4,16 @@ package com.example.demo5;
 public class Account {
     private String login;
     private String password;
-
-
-    public void InitAccount(String login, String password){
+    private boolean admin;
+    private String email;
+    public Account(String login, String password, String email){
         this.login = login;
         this.password = password;
+        admin = false;
+    }
+
+    public void SetAdmin(){
+        this.admin = true;
     }
     public String getLogin(){
         return login;
