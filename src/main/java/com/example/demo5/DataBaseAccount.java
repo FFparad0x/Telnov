0bd07bf50981078f12ab68a2f6218a735e9beb50
Parad0x <viktor3755@gmail.com>
2021-12-14 12:24:45 +0300

    udp

--- src/main/java/com/example/demo5/DataBaseAccount.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo5;
-
-import java.util.ArrayList;
-
-public class DataBaseAccount {
-    public static ArrayList<Account> accountиs;
-}
--- src/main/java/com/example/demo5/DataBaseTicket.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo5;
-
-import java.util.ArrayList;
-
-public class DataBaseTicket {
-    public static ArrayList<Ticket> tickets;
-}
